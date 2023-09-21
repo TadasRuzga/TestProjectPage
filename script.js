bbee3ccd26bc648a2034808fd8314a1575eba07f
Tadas Ruzga <tadasruzga94@gmail.com>
2023-09-21 21:11:21 +0100

    Some changes

--- script.js
+++ script.js
@@ -3,48 +3,47 @@ document.addEventListener("DOMContentLoaded", function () {
   const message = document.getElementById("message");
 
   registrationForm.addEventListener("submit", function (event) {
-      event.preventDefault();
+    event.preventDefault();
 
-      const username = document.getElementById("username").value;
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-      const password1 = document.getElementById("password1").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const password1 = document.getElementById("password1").value;
 
-      if (password !== password1) {
-          message.textContent = "Passwords do not match, please try again.";
-          return; 
-      }
+    if (password !== password1) {
+      message.textContent = "Passwords do not match, please try again.";
+      return;
+    }
 
-      const userData = {
-          username: username,
-          email: email,
-          password: password,
-      };
+    const userData = {
+      username: username,
+      email: email,
+      password: password,
+    };
 
-      localStorage.setItem(username, JSON.stringify(userData));
-      message.textContent = "Registration successful!";
-      registrationForm.reset();
+    localStorage.setItem(username, JSON.stringify(userData));
+    message.textContent = "Registration successful!";
+    registrationForm.reset();
   });
 });
 
-
 // Random color after click on the body
 document.addEventListener("DOMContentLoaded", function () {
-  const htmlBody = document.querySelector('#wotlk-body'); 
-  const colors = ["#7393B3", "#808080"];
-  let usedColors = []; 
+  const htmlBody = document.querySelector("#wotlk-body");
+  const colors = ["rgb(80, 151, 164)", "rgb(149, 200, 216)"];
+  let usedColors = [];
 
   const randomClickFunction = function () {
-      if (colors.length === 0) {
-          colors.push(...usedColors);
-          usedColors = [];
-      }
+    if (colors.length === 0) {
+      colors.push(...usedColors);
+      usedColors = [];
+    }
 
-      const randomIndex = Math.floor(Math.random() * colors.length);
-      const randomColor = colors.splice(randomIndex, 1)[0]; 
-      usedColors.push(randomColor); 
-      htmlBody.style.backgroundColor = randomColor;
-  }
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors.splice(randomIndex, 1)[0];
+    usedColors.push(randomColor);
+    htmlBody.style.backgroundColor = randomColor;
+  };
 
   htmlBody.onclick = randomClickFunction;
   randomClickFunction();
